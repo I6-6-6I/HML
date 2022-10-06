52b7e659b2cd214d7a4b12090d7acd7cef374dc9
6 6 6 <chomiczjakub80@gmail.com>
2022-10-06 20:49:41 +0200

    Changed the way user interacts with Types

--- HML/defines/user_type_defs.hpp
+++ HML/defines/user_type_defs.hpp
@@ -26,11 +26,18 @@ HEADER_MATH_NAMESPACE
 
 	//Declares----------------------------------------------------------------------------------------------------------------------
 	#pragma region DECLARES
-	DECLARE_VECTOR1
-	DECLARE_VECTOR2
-	DECLARE_VECTOR3
-	DECLARE_VECTOR4
-
-	DECLARE_MAT2
+	template <typename T = float> using Vector1		= vector1_t<T>;
+	template <typename T = float> using Vector2		= vector2_t<T>;
+	template <typename T = float> using Vector3		= vector3_t<T>;
+	template <typename T = float> using Vector4		= vector4_t<T>;
+	template <typename T = float> using Mat2x2		= matrix2x2_t<T>;
+	template <typename T = float> using Mat2x3		= matrix2x3_t<T>;
+	template <typename T = float> using Mat2x4		= matrix2x4_t<T>;
+	template <typename T = float> using Mat3x2		= matrix3x2_t<T>;
+	template <typename T = float> using Mat3x3		= matrix3x3_t<T>;
+	template <typename T = float> using Mat3x4		= matrix3x4_t<T>;
+	template <typename T = float> using Mat4x2		= matrix4x2_t<T>;
+	template <typename T = float> using Mat4x3		= matrix4x3_t<T>;
+	template <typename T = float> using Mat4x4		= matrix4x4_t<T>;
 	#pragma endregion
 }
