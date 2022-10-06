9c207583998d11be31061c866ca1e7638d862ea1
6 6 6 <chomiczjakub80@gmail.com>
2022-10-06 20:49:58 +0200

    Deleted unnecessary includes

--- HML/defs.hpp
+++ HML/defs.hpp
@@ -1,10 +1,4 @@
 #pragma once
 #include "defines/core_defs.hpp"
-#include "defines/vector1_defs.hpp"
-#include "defines/vector2_defs.hpp"
-#include "defines/vector3_defs.hpp"
-#include "defines/vector4_defs.hpp"
-
-#include "defines/matrix2_defs.hpp"
 
 #include "defines/user_type_defs.hpp"
